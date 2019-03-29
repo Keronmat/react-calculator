13af12f069d061bab94c6f645d9acd9725a4d78c
keronmat <keronmat@live.com>
2019-03-28 22:13:33 -0400

    Updates

---
+++ precache-manifest.09357b587478088c25dcd9a661887195.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  },
+  {
+    "revision": "5b431b942ba6641fc73c",
+    "url": "/react-calculator/static/js/main.e0cb90a1.chunk.js"
+  },
+  {
+    "revision": "801faef9ca2fe6c56cbc",
+    "url": "/react-calculator/static/js/2.0fc45b1f.chunk.js"
+  },
+  {
+    "revision": "5b431b942ba6641fc73c",
+    "url": "/react-calculator/static/css/main.cb8a7af2.chunk.css"
+  },
+  {
+    "revision": "801faef9ca2fe6c56cbc",
+    "url": "/react-calculator/static/css/2.3d49f4f1.chunk.css"
+  },
+  {
+    "revision": "8bae4ffac46c8831faae582d0452824d",
+    "url": "/react-calculator/index.html"
+  }
+];
